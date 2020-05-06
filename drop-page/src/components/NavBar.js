cb73b5a4c1a6fa8acffcddd2cf0ab690caa01a2e
Dan Cripps <dan-cripps@hotmail.co.uk>
2020-05-06 15:20:09 +0100

    additional styling added

--- drop-page/src/components/NavBar.js
+++ drop-page/src/components/NavBar.js
@@ -4,22 +4,18 @@ class NavBar extends Component {
 
     render( ) {
         return (
-            <div className='header--Container'>
-                <div className='link--Container'>
-                    <div className='header--box1'>
-                        <h1>Daniel Cripps</h1>
-                    </div>
-                    <div className='header--box2'>
-                        <h1> Pricing</h1>
-                    </div>
-                    <div className='header--box3'>
-                        <h1> Solutions</h1>
-                    </div>  
-                    <div className='header--box4'>
-                        <h1> Sign In</h1>
-                    </div>  
-                </div>
-            </div>
+            <header className='header--Container'>
+                <nav className='link--Container'>
+                    <div className='header--box1'><h1>Daniel Cripps</h1></div>
+                    <div className='navbar_items'>
+                        <ul>
+                            <li> Pricing</li>
+                            <li> Services</li>
+                            <li> Sign in</li>
+                        </ul>
+                    </div> 
+                </nav>
+            </header>
         )
     }
 
