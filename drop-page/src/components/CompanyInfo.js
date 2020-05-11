3f31e3326ea0acb880298d20db18221d47d4d318
Dan Cripps <dan-cripps@hotmail.co.uk>
2020-05-11 19:12:36 +0100

    flex css on signup div

--- drop-page/src/components/CompanyInfo.js
+++ drop-page/src/components/CompanyInfo.js
@@ -10,7 +10,8 @@ export default class CompanyInfo extends Component {
                         <p className='depth--container'>NE are at the forefront of bridging the gap between new Esports teams with the correct organisation in order for them to gain the and have access to the most relevant individuals and opportunities within the market.</p>
                     </div>
                     <div className='signup--container'>
-                    <input type="text" name="username" placeholder="email address" required="required" />
+                        <p>Enter Email Address for free consultation</p>
+                        <input type="text" name="username" placeholder="email address" required="required" />
                     </div>
                     <div className='homepageimage--container'><img src="/images/homepage.png" alt=""/> </div>
                 </div>
