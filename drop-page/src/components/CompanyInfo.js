1bdcb9e55a08a2950f64decbe2f9268220745bd2
Dan Cripps <dan-cripps@hotmail.co.uk>
2020-05-11 19:06:43 +0100

    input fields for sign in created

--- drop-page/src/components/CompanyInfo.js
+++ drop-page/src/components/CompanyInfo.js
@@ -7,7 +7,10 @@ export default class CompanyInfo extends Component {
                 <div className='info--container'> 
                     <div className='banner--container'>
                         <h1>Deriving innovative solutions and support for your team.</h1>
-                        <p className='depth--container'>NE are at the forefront of bridging the gap between new Esports teams with the correct organisation in order for them to gain the and have access to the correct individuals and opportunities within the market.</p>
+                        <p className='depth--container'>NE are at the forefront of bridging the gap between new Esports teams with the correct organisation in order for them to gain the and have access to the most relevant individuals and opportunities within the market.</p>
+                    </div>
+                    <div className='signup--container'>
+                    <input type="text" name="username" placeholder="email address" required="required" />
                     </div>
                     <div className='homepageimage--container'><img src="/images/homepage.png" alt=""/> </div>
                 </div>
