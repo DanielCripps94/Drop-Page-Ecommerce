866d2eae8e57c62558b8e49c8c7e9da7432f6c97
Dan Cripps <dan-cripps@hotmail.co.uk>
2020-05-12 16:00:29 +0100

    styling on imported images - flex - team component

--- drop-page/src/components/TeamInfo.js
+++ drop-page/src/components/TeamInfo.js
@@ -4,8 +4,14 @@ export default class TeamInfo extends Component {
     render() {
         return ( 
             <div className='team--container'>
+                <div className='gap'>   - </div>
                 <div className='team--title--container'>
-                <p>What we can offer to teams</p>
+                <h1>What we can offer to teams</h1>
+                </div>
+                <div className='team--image--container'>
+                <img src="/images/team1.png" alt=""/>
+                <img src="/images/team2.png" alt=""/>
+                <img src="/images/team3.png" alt=""/> 
                 </div>
             </div>
         )
