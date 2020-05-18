3c63b5f394c5b4d6e905b3d4b12eb2f17a39ee12
Dan Cripps <dan-cripps@hotmail.co.uk>
2020-05-18 17:29:28 +0100

    option objects created - state for class set

--- drop-page/src/components/SelectBox.js
+++ drop-page/src/components/SelectBox.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import select from 'react-select'
+
+const options = [
+    {value: 'Team', label: 'Team'},
+    {value: 'Organisation', label: 'Organisation'}
+]
 
 export default class selectBox extends component {
+
+    state = {
+        SelectedOption: null
+    }
+
     render() {
         return (
             <div>
-                
+
             </div>
         )
     }
